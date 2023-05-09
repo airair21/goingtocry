eb4e0a820ecd5986e1c1617ac7f85082e9b882ca
jax wong <113402506+airair21@users.noreply.github.com>
2023-05-09 14:44:25 -0400

    GOD FUCK ME

--- albumDetail.js
+++ albumDetail.js
@@ -11,6 +11,7 @@ function renderAlbumDetail(album) {
   // Set the title of the page to the title of the album
   document.querySelector('h1').innerText = album.Station_Name
   document.querySelector('h2').innerText = album.Art_Title
+  document.querySelector('h3').innerText = album.Lines
   
   // Create an image element to display the cover image of the album
   // const img = document.createElement('img')
